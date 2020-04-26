6fef32d7ba1ee2d0c956d470cdecf687a3acae4f
Rohith <52905139+rohopt@users.noreply.github.com>
2020-04-26 21:11:36 +0530

    add morestrings

--- hello.go
+++ hello.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/rohopt/hello/morestrings"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println(morestrings.ReverseRunes("Hello, Go!"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
